0d7eb60c79289e2dc17793ec3f20667993af0244
Chris Burnham <cburnham@greensea.com>
2020-02-14 11:26:13 -0500

    adding pthreads function with copy of serial

--- linear_equations.c
+++ linear_equations.c
@@ -86,3 +86,64 @@ int gaussian_solve( int size, floating_type *a, floating_type *b )
         return_code = back_substitution( size, a, b );
     return return_code;
 }
+
+/**
+ * @brief gaussian_solve_pthreads Do gausian elimination using multiple threads
+ * @param size of the input
+ * @param a matrix of the inputs
+ * @param b vector of the outputs
+ * @return error code. 0 on sucsess, negative on failure
+ */
+int gaussian_solve_pthreads(int size, floating_type *a, floating_type *b)
+{
+  floating_type *temp_array =
+      (floating_type *)malloc( size * sizeof(floating_type) );
+  int            i, j, k;
+  floating_type  temp, m;
+
+  for( i = 0; i < size - 1; ++i )
+  {
+      // Find the row with the largest value of |a[j][i]|, j = i, ..., n - 1
+      k = i;
+      m = fabs( MATRIX_GET( a, size, i, i ) );
+      for( j = i + 1; j < size; ++j )
+      {
+          if( fabs( MATRIX_GET( a, size, j, i ) ) > m )
+          {
+              k = j;
+              m = fabs( MATRIX_GET( a, size, j, i ) );
+          }
+      }
+
+      // Check for |a[k][i]| zero.
+      if( fabs( MATRIX_GET( a, size, k, i ) ) <= 1.0E-6 )
+      {
+          return -2;
+      }
+
+      // Exchange row i and row k, if necessary.
+      if( k != i )
+      {
+          memcpy( temp_array, MATRIX_GET_ROW( a, size, i ), size * sizeof( floating_type ) );
+          memcpy( MATRIX_GET_ROW( a, size, i ), MATRIX_GET_ROW( a, size, k ), size * sizeof( floating_type ) );
+          memcpy( MATRIX_GET_ROW( a, size, k ), temp_array, size * sizeof( floating_type ) );
+
+          // Exchange corresponding elements of b.
+          temp = b[i];
+          b[i] = b[k];
+          b[k] = temp;
+      }
+
+      // Subtract multiples of row i from subsequent rows.
+      for( j = i + 1; j < size; ++j )
+      {
+          m = MATRIX_GET( a, size, j, i ) / MATRIX_GET( a, size, i, i );
+          for( k = 0; k < size; ++k )
+          {
+              MATRIX_PUT( a, size, j, k, MATRIX_GET( a, size, j, k ) - m * MATRIX_GET( a, size, i, k ) );
+          }
+          b[j] -= m * b[i];
+      }
+  }
+  return 0;
+}
