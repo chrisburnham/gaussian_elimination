5131c5465fd09bebe57f3ed64ab2f7397ffb25a3
Chris Burnham <cburnham@greensea.com>
2020-05-17 09:58:23 -0400

    back to h

--- cuda_elim.cu
+++ cuda_elim.cu
@@ -1,26 +1,26 @@
-#include "cuda_elim.h"
-#include <cuda_runtime.h>
+// #include "cuda_elim.h"
+// #include <cuda_runtime.h>
 
-__global__ void cuda_sub_mul_kernel(int size,
-								     							  floating_type* matrix,
-															      floating_type* vector)
-{
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-  int j = blockIdx.y * blockDim.y + threadIdx.y;
+// __global__ void cuda_sub_mul_kernel(int size,
+// 								     							  floating_type* matrix,
+// 															      floating_type* vector)
+// {
+// 	int i = blockIdx.x * blockDim.x + threadIdx.x;
+//   int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-	MATRIX_PUT(matrix, size, i, j, 1);
-	vector[i] = 2;
-}
+// 	MATRIX_PUT(matrix, size, i, j, 1);
+// 	vector[i] = 2;
+// }
 
-void cuda_subtract_multiples(const int size,
-							 						   floating_type* matrix,
-							 							 floating_type* vector)
-{
-	//double card_mat[size][size];
-	//double card_vect[size];
-	//double card_output[size];
+// void cuda_subtract_multiples(const int size,
+// 							 						   floating_type* matrix,
+// 							 							 floating_type* vector)
+// {
+// 	//double card_mat[size][size];
+// 	//double card_vect[size];
+// 	//double card_output[size];
 
-	dim3 threads_per_block(100,100);
-	dim3 num_blocks(size / threads_per_block.x, size / threads_per_block.y);
-	cuda_sub_mul_kernel<<<num_blocks, threads_per_block>>>(size, matrix, vector);
-}
+// 	dim3 threads_per_block(100,100);
+// 	dim3 num_blocks(size / threads_per_block.x, size / threads_per_block.y);
+// 	cuda_sub_mul_kernel<<<num_blocks, threads_per_block>>>(size, matrix, vector);
+// }
